105923070de5c44846beac5de5d3c1d785497ba3
Chris <christophersstjohn@gmail.com>
2018-01-21 01:01:16 -0800

    Animations (examples)

--- ccmarket/src/containers/ContentSection/ContentSection.js
+++ ccmarket/src/containers/ContentSection/ContentSection.js
@@ -3,20 +3,21 @@ import React, { Component } from 'react';
 import DataTickerApi from '../../common/DataTickerApi/DataTickerApi';
 import './ContentSection.css';
 
-
 const ContentSection = (props) => {
    return (
      <div className="ContentSection--container">
 
       <div className="ContentSection--header">
-      Coin.API.cc Market Tracker
+        Coin.API.cc Market Tracker
       </div>
+
+      <div className="ContentSection--subheader fadein">Watch this page for Coin market updates.</div>
 
       <div className="ContentSection--intro">
         
-      Cryptocurrencies are used primarily outside existing banking and governmental institutions and are exchanged over the Internet. While these alternative, decentralized modes of exchange are in the early stages of development, they have the unique potential to challenge existing systems of currency and payments. As of December 2017 total market capitalization of cryptocurrencies is bigger than 600 billion USD and record high daily volume is larger than 500 billion USD.
+        Cryptocurrencies are used primarily outside existing banking and governmental institutions and are exchanged over the Internet. While these alternative, decentralized modes of exchange are in the early stages of development, they have the unique potential to challenge existing systems of currency and payments. As of December 2017 total market capitalization of cryptocurrencies is bigger than 600 billion USD and record high daily volume is larger than 500 billion USD.
 
-      
+        
 
       </div>
 
