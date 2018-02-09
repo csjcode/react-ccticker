8520e16b45bd680491f26eaf5fd07f83a3dd8a7c
Chris <christophersstjohn@gmail.com>
2018-02-09 14:16:51 -0800

    Update css on ContentSection

--- ccmarket/src/containers/ContentSection/ContentSection.js
+++ ccmarket/src/containers/ContentSection/ContentSection.js
@@ -15,10 +15,13 @@ const ContentSection = (props) => {
 
       <div className="ContentSection--intro">
         
-        Cryptocurrencies are used primarily outside existing banking and governmental institutions and are exchanged over the Internet. While these alternative, decentralized modes of exchange are in the early stages of development, they have the unique potential to challenge existing systems of currency and payments. As of December 2017 total market capitalization of cryptocurrencies is bigger than 600 billion USD and record high daily volume is larger than 500 billion USD.
+        <p className="large-content">
+          Cryptocurrencies are used primarily outside existing banking and governmental institutions and are exchanged over the Internet. While these alternative, decentralized modes of exchange are in the early stages of development, they have the unique potential to challenge existing systems of currency and payments. 
+        </p>
+        <p>
+          As of December 2017 total market capitalization of cryptocurrencies is bigger than 600 billion USD and record high daily volume is larger than 500 billion USD.
 
-        
-
+        </p>
       </div>
 
       <div className="ContentSection--price-table">
