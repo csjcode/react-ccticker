c1d1e00009a5df046ddcd31f9e0015d88685ec4a
Chris <christophersstjohn@gmail.com>
2018-03-02 21:36:03 -0800

    RouterIndex injections and NewsPage

--- ccmarket/src/pages/RouterIndex.js
+++ ccmarket/src/pages/RouterIndex.js
@@ -5,6 +5,7 @@ import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import HeaderSection from '../containers/HeaderSection/HeaderSection';
 import FooterSection from '../containers/FooterSection/FooterSection';
 import CoinListPage from './CoinListPage/CoinListPage';
+import NewsPage from './NewsPage/NewsPage';
 import App from '../containers/App';
 import logo from '../images/logo.svg';
 import grey from 'material-ui/colors/grey';
@@ -28,13 +29,13 @@ const RouterIndex = (props) => {
             {/* <AlertSection /> */}
             <HeaderSection />
             <Switch>
-               <Route exact path="/" component={App}/>
-               <Route path="/market" component={App}/>
+               <Route exact path="/" render={()=><App pageOn="app" />}/>
+               <Route path="/market" render={()=><App pageOn="app" />}/>
                <Route path="/coinlist" render={()=><CoinListPage pageOn="coinlist" />}/>
-               <Route path="/news" component={App}/>
-               <Route path="/blog" component={App}/>
-               <Route path="/updates" component={App}/>
-               <Route path="/faq" component={App}/>
+               <Route path="/news" render={()=><NewsPage pageOn="news"/>}/>
+               <Route path="/blog" render={()=><App pageOn="app" />}/>
+               <Route path="/updates" render={()=><App pageOn="app" />}/>
+               <Route path="/faq" render={()=><App pageOn="app" />}/>
             </Switch>
             <FooterSection />
          </div>
