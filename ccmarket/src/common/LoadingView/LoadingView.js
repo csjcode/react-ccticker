eb0b2c3e87af35cf8ec1509046f35955322bac21
Chris <christophersstjohn@gmail.com>
2018-03-03 00:03:47 -0800

    Updated Loader graphic/component

--- ccmarket/src/common/LoadingView/LoadingView.js
+++ ccmarket/src/common/LoadingView/LoadingView.js
@@ -3,7 +3,16 @@ import './LoadingView.css';
 
 const LoadingView = () => {
 	return(
-		<div className="LoadingView--container">Loading...</div>
+		// <div className="LoadingView--container">Loading...</div>
+		<div className="LoadingView--container">
+			<p>Loading...</p>
+			<div class="loading">
+				<div class="loading-bar"></div>
+				<div class="loading-bar"></div>
+				<div class="loading-bar"></div>
+				<div class="loading-bar"></div>
+			</div>
+		</div>
 	);
  }
 
