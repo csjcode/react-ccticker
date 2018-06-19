a5aaec74a601a5649d7727e713a1dede20d58ecf
Chris <christophersstjohn@gmail.com>
2018-06-18 22:04:36 -0700

    Server change to Nginx reverse proxy config

--- ccmarket/server/server.js
+++ ccmarket/server/server.js
@@ -8,36 +8,46 @@ const querystring = require('querystring');
 const compression = require('compression');
 const bodyParser = require('body-parser');
 
+https.createServer({ 
+  key: fs.readFileSync("/etc/letsencrypt/live/coin.api.cc/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/coin.api.cc/fullchain.pem")
+}, app).listen(443);
+
 
 app.use(compression()); 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
+
 function ensureSecure(req, res, next){
+  
+  console.log(req.hostname + ' ' + req.protocol);
+
   if(req.secure){
     // OK, continue
+    console.log('secure');
+    app.get('/*', function (req, res) {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+    
+    app.get('/charts/range/*', function (req, res) {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+     });
     return next();
-  };
-  res.redirect('https://'+req.host+req.url); // handle port numbers if non 443
+  } else {
+    console.log('redirecting');
+    return res.redirect(301,'https://coin.api.cc'+req.url); // handle port numbers if non 443
+  }
 };
 
-app.all('*', ensureSecure);
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.all('/*', ensureSecure);
 
-app.get('/charts/range/*', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
+
 
 
 //app.listen(3000);
-app.listen(80);
+//app.listen(8080);
 
 // ca: fs.readFileSync("/etc/letsencrypt/archive/example.com/chain1.pem")
 
-https.createServer({ 
-        key: fs.readFileSync("/etc/letsencrypt/live/coin.api.cc/privkey.pem"),
-        cert: fs.readFileSync("/etc/letsencrypt/live/coin.api.cc/fullchain.pem")
-}, app).listen(443);
